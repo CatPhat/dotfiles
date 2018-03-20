18c06aad834aef47683d470a5010366c1474fb05
Kian Movasagi <kian@vedos.io>
2018-03-20 12:46:00 -0600

    remove dtodo dlog files and fish functions

--- .config/fish/functions/d-todo.fish
+++
@@ -1,7 +0,0 @@
-function d-todo
-	set -l todo ~/dotfiles/dtodo
-	echo "BEGIN LOG ENTRY - " (date +%Y%m%d%H%M%S) >> $todo
-	echo $argv >> $todo
-	echo "END LOG ENTRY" >> $todo
-	echo "" >> $todo
-end
