2735f966b206aec0d5964399788a5221a4714675
Kian Movasagi <kian@vedos.io>
2019-01-16 18:04:53 -0700

    add docker container env vars

--- setup/tests/run-tests.sh
+++ setup/tests/run-tests.sh
@@ -8,8 +8,8 @@ cd $DOTFILES_ROOT
 
 debug "\$DOTFILES_ROOT: ${DOTFILES_ROOT}"
 info "Building Dockerfile"
-docker build --build-arg dotfiles_root=${DOTFILES_ROOT} -f ${DOTFILES_ROOT}/setup/tests/Dockerfile --tag bats/bats:latest .
+docker build --no-cache --build-arg dotfiles_root=${DOTFILES_ROOT} -f ${DOTFILES_ROOT}/setup/tests/Dockerfile --tag bats/bats:latest .
 
 info "Running bats-core unit tests"
 
-docker run -it --rm --workdir ${DOTFILES_ROOT} bats/bats:latest -t setup/tests
+docker run -it --rm -e DOTFILES_ROOT=${DOTFILES_ROOT} --workdir ${DOTFILES_ROOT} bats/bats:latest -t setup/tests
