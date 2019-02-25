06e6d937c2c49705e8a9fc78bfa07d107052bfc4
Kian Movasagi <kian@vedos.io>
2019-02-25 13:29:34 -0700

    add git to arch setup prequisities

--- setup/setup_prequisites.arch.sh
+++ setup/setup_prequisites.arch.sh
@@ -4,7 +4,7 @@ sudo rm -fr /etc/pacman.d/gnupg \
     && sudo pacman-key --init \
     && sudo pacman-key --populate archlinux \
     && sudo pacman -Syu --needed --noconfirm \
-       base base-devel reflector
+       base base-devel reflector git
 
 # Update package mirrorlist, use fastest mirrors available
 sudo reflector --verbose --latest 40 --number 10 --sort rate --protocol http --save /etc/pacman.d/mirrorlist
