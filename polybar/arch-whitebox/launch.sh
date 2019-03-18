68d33509b7b1efd5013e906b5a1c7a3a873efedf
Kian Movasagi <kian@vedos.io>
2019-03-18 15:00:16 -0600

    remove polybar common folder

--- polybar/arch-whitebox/launch.sh
+++ polybar/arch-whitebox/launch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Termite all instances
 killall -q polybar
@@ -8,3 +8,5 @@ while pgrep -U $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch
 polybar main &
+polybar left &
+polybar right &
