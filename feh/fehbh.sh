d2d77a8b19a322161d5ea8b77822e5495255624a
catphat <kian@vedos.io>
2019-06-19 17:05:57 -0600

    add feh

---
+++ feh/fehbh.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+feh \
+    --bg-fill "${HOME}/.wallpapers/horizontal/by_localservers2_2560.jpg" \
+    --bg-fill "${HOME}/.wallpapers/vertical/by_warmachines8_1920.jpg" \
+    --bg-fill "${HOME}/.wallpapers/horizontal/by_gathering3_2880.jpg"
